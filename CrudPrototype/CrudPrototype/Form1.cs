82c75206ea059cd2dd10b97e64f5b24689bcca33
Cooper, Drew <DrewCooper@quickenloans.com>
2017-09-06 18:05:55 -0400

    example branch

--- CrudPrototype/CrudPrototype/Form1.cs
+++ CrudPrototype/CrudPrototype/Form1.cs
@@ -16,5 +16,10 @@ namespace CrudPrototype
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("example box");
+        }
     }
 }
